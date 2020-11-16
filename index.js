1da943c41907f62bb72f721c48e9ae593e7b0904
FrancoisBasset <>
2020-11-16 18:26:05 +0100

    maj

--- index.js
+++ index.js
@@ -11,25 +11,25 @@ const NodeRandomArticle = require('./classes/NodeRandomArticle');
 app.use(express.static('./public'));
 
 app.listen(80, function() {
-    console.log('Start on 80');
+	console.log('Start on 80');
 });
 
 app.get('/', function(req, res) {
-    res.sendFile('./index.html');
+	res.sendFile('./index.html');
 });
 
 app.get('/search', function(req, res) {
-    const article = new NodeArticle(req.query.url);
-
-    article.fetch().then(function() {
-        res.json(article);
-    });
+	const article = new NodeArticle(req.query.url);
+	
+	article.fetch().then(function() {
+		res.json(article);
+	});
 });
 
 app.get('/searchRandom', function(req, res) {
-    const article = new NodeRandomArticle();
-    
-    article.fetch().then(function() {
-        res.json(article);
-    });
-})
+	const article = new NodeRandomArticle();
+	
+	article.fetch().then(function() {
+		res.json(article);
+	});
+});
